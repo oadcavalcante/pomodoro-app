37b1e2b5e4d02c30a01bf61971560f860cb720e3
adcavalcant <aderc19@gmail.com>
2024-02-05 10:44:55 -0300

    :lipstick: feat: Estilizações Iniciais.

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -4,11 +4,14 @@ import { Button } from './button';
 import { Timer } from './timer';
 
 interface Props {
-  defaultPomodoroTimer: number;
+  pomodoroTimer: number;
+  shortRestTime: number;
+  longRestTime: number;
+  cycles: number;
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
-  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTimer);
+  const [mainTime, setMainTime] = React.useState(props.pomodoroTimer);
   useInterval(() => {
     setMainTime(mainTime - 1);
   }, 1000);
@@ -17,7 +20,18 @@ export function PomodoroTimer(props: Props): JSX.Element {
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
-      <Button text="teste" onClick={() => console.log(1)}></Button>
+
+      <div className="controls">
+        <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button text="teste" onClick={() => console.log(1)}></Button>
+      </div>
+
+      <div className="details">
+        <p>Teste</p>
+        <p>Teste</p>
+        <p>Teste</p>
+      </div>
     </div>
   );
 }
